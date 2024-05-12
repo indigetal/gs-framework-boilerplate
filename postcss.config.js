b9e143a1a25f9794a3643f543b8f450e21b2b25c
Brandon Meyer <brandon@indigetal.com>
2024-05-12 19:23:19 -0400

    removed tokens-utility

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   plugins: [
-    require('postcss-preset-env')({
+    require("postcss-preset-env")({
       autoprefixer: {
         grid: true,
       },
       stage: 3,
-      browsers: 'last 5 versions', // Add browser support configuration here
+      browsers: "last 5 versions", // Add browser support configuration
     }),
-    require('postcss-import-ext-glob'),
-    require('postcss-import')({
-      path: ['assets/css/src'], // Specify the directory containing your CSS files
-      to: 'assets/css/dist' // Specify the output directory for processed CSS
+    require("postcss-import-ext-glob"),
+    require("postcss-import")({
+      path: ["assets/css/src"], // Specify the directory containing your CSS files
+      to: "assets/css/dist", // Specify the output directory for processed CSS
     }),
   ],
 };
